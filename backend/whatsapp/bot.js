d8e2efc45a965539c4e0238976cf54146420554e
Srishti Verma <136918651+lost-pika@users.noreply.github.com>
2026-02-08 01:40:13 +0530

    Add executable path for Puppeteer

--- backend/whatsapp/bot.js
+++ backend/whatsapp/bot.js
@@ -22,6 +22,7 @@ const initBot = async () => {
     }),
     puppeteer: {
       headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
